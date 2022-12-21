8a1c8091fef5d7be2b16f438d1d359986f92e443
Liam Stevens <8955671+liamstevens111@users.noreply.github.com>
2022-12-22 00:10:07 +0700

    Setup redis connection wip

--- lib/terraform_elixir_repo_web/controllers/page_controller.ex
+++ lib/terraform_elixir_repo_web/controllers/page_controller.ex
@@ -1,8 +1,19 @@
-# credo:disable-for-this-file CompassCredoPlugin.Check.DoSingleExpression
+# credo:disable-for-this-file
 defmodule TerraformElixirRepoWeb.PageController do
   use TerraformElixirRepoWeb, :controller
+  alias Redix
 
   def index(conn, _params) do
+    Redix.command(:redix, ["PING"])
+    Redix.command(:redix, ["SET", "mykey", "foo"])
+    result = Redix.command(:redix, ["GET", "mykey"])
+    IO.inspect(result)
+    IO.puts("result has finshed")
+    inspect(result)
+
+    output = System.get_env("REDIS_ENDPOINT_ADDRESS")
+    IO.inspect(output)
+
     render(conn, "index.html")
   end
 end
