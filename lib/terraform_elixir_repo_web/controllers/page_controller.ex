085eab36fa66c7597a19b7b6ff953d2cd65e8563
Liam Stevens <8955671+liamstevens111@users.noreply.github.com>
2022-12-22 01:50:04 +0700

    [#5] [#6] Display redis values and environment variables on home page (#9)
    * Display redis values and environment variables
    
    * Display redis values and environment variables
    
    * Remove coverage+tests for home page
    
    * Remove coverage+tests for home page
    
    * Remove coverage+tests for home page

--- lib/terraform_elixir_repo_web/controllers/page_controller.ex
+++ lib/terraform_elixir_repo_web/controllers/page_controller.ex
@@ -3,17 +3,25 @@ defmodule TerraformElixirRepoWeb.PageController do
   use TerraformElixirRepoWeb, :controller
   alias Redix
 
-  def index(conn, _params) do
-    Redix.command(:redix, ["PING"])
-    Redix.command(:redix, ["SET", "mykey", "foo"])
-    result = Redix.command(:redix, ["GET", "mykey"])
-    IO.inspect(result)
-    IO.puts("result has finshed")
-    inspect(result)
+  def index(conn, params) do
+    {previous_value, new_value} =
+      if params["redis-key"] do
+        previous_value = Redix.command(:redix, ["GET", "redis_key"])
+        Redix.command(:redix, ["SET", "redis-key", params["redis-key"]])
+        new_value = Redix.command(:redix, ["GET", "redis_key"])
+        {previous_value, new_value}
+      else
+        {nil, nil}
+      end
 
-    output = System.get_env("REDIS_ENDPOINT_ADDRESS")
-    IO.inspect(output)
+    redis_endpoint = System.get_env("REDIS_ENDPOINT_ADDRESS")
+    s3_bucket_name = System.get_env("AWS_S3_BUCKET_NAME")
 
-    render(conn, "index.html")
+    render(conn, "index.html",
+      previous_value: previous_value,
+      new_value: new_value,
+      redis_endpoint: redis_endpoint,
+      s3_bucket_name: s3_bucket_name
+    )
   end
 end
